24626e877bf746a82eac7af1dae978917bf85e5d
Chris Harrison <cdharrison@me.com>
2013-06-01 18:02:44 -0400

    Added logo to header

--- public_html/inc/header.php
+++ public_html/inc/header.php
@@ -42,7 +42,6 @@
 		
 		<header class="jumbotron subhead" id="overview">
 			<div class="container">
-				<h1>Water Purity</h1>
-				<p class="lead">Some text will go here.</p>
+				<img src="img/logo-cleanwater.png" alt="Clean Water" />
 			</div>
 		</header>
