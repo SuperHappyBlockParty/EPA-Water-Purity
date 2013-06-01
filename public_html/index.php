78526945733db7f9eb44b1101aa48e503fda700b
Chris Harrison <cdharrison@me.com>
2013-06-01 19:47:08 -0400

    First search integration on index.

--- public_html/index.php
+++ public_html/index.php
@@ -15,8 +15,8 @@
 				<div class="span11">
 					<h1>See if water quality issues exist in your area.</h1>
 					<p>Enter your zip code:</p>
-					<form class="form-search" method="post" action="">
-						<input type="text" class="input-large search-query">
+					<form class="form-search" method="post" action="/">
+						<input type="text" class="input-large search-query" name="zip" />
 						<button type="submit" class="btn btn-medium"><i class="icon-search"></i> Search</button>
 					</form>
 				</div>
@@ -25,17 +25,44 @@
 		<div class="container">
 			<div class="row">
 				<div class="span12">
+					<?php
+					if (strlen($_REQUEST['zip']) > 4) {
+						if (is_array($result_data) && count($result_data) > 0) {
+							echo '<table class="table table-striped">\n';
+							echo "<tr>\n";
+							echo '<th>Facility Name</th>';
+							echo '<th>County Served</th>';
+							echo '<th>Contaminant</th>';
+							echo '<th>Health Effects</th>';
+							echo '<th>Sources</th>';
+							echo '<th>Name</th>';
+							echo "</tr>\n";
+							foreach ($result_data as $k => $v) {
+								//print_r($v); // Uncomment to display all data
+								echo "<tr>\n";
+								echo '<td>' . $v['PWSNAME'] . "</td>\n"; // Display a single column
+								echo '<td>' . $v['COUNTYSERVED'] . "</td>\n";
+								echo '<td>' . $v['CNAME'] . "</td>\n";
+								echo '<td>' . $v['HEALTH_EFFECTS'] . "</td>\n";
+								echo '<td>' . $v['SOURCES'] . "</td>\n";
+								echo "</tr>\n";
+								echo "<tr>\n";
+								echo '<td colspan="5">' . $v['DEFINITION'] . "</td>\n";
+								echo "</tr>\n";
+							}
+							echo '</table>';
+						} else {
+							echo '<h1>No data was available.</h1>';
+						}
+					}
 					
+					?>
 				</div>
 			</div>
 		</div>
 		
 		<div>
-			<?php
-			foreach ($result_data as $k => $v) {
-				echo $v[''];
-			}
 			
-			?>
+			
 		</div>
 <?php include 'inc/footer.php'; ?>
