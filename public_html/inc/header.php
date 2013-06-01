9aa5e8151809b0818a291051b6dc3ae7116a98f1
Chris Harrison <cdharrison@me.com>
2013-06-01 18:19:50 -0400

    Favicon awesomeness!

--- public_html/inc/header.php
+++ public_html/inc/header.php
@@ -14,6 +14,13 @@
 		<!--[if lt IE 9]>
 		  <script src="js/html5shiv.js"></script>
 		<![endif]-->
+		
+		<!-- Le fav and touch icons -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/ico/apple-touch-icon-114-precomposed.png">
+		  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/ico/apple-touch-icon-72-precomposed.png">
+						<link rel="apple-touch-icon-precomposed" href="img/ico/apple-touch-icon-57-precomposed.png">
+									   <link rel="shortcut icon" href="img/ico/favicon.png">
 
 	</head>
 
