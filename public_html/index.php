8d86eaad0ee482cf2cdf71804f7be4f5defca33b
Chris Harrison <cdharrison@me.com>
2013-06-01 15:28:45 -0400

    Integrated bootstrap into base working file. Front-end work + other assets.

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sample PHP Document</title>
-
-<style type="text/css">
-	TH {
-		background-color: #006;
-		color: #FFF;
-	}
-</style>
-
 <?php
 // phpinfo();
 // this method requests XML data from the REST service
@@ -44,7 +31,7 @@ function requestData($url) {
 
 // this method displays the XML results in a simple table
 function resultsToTable($results) {
-	$html = "<table border='1' cellspacing='0' cellpadding='2'>\n";
+	$html = "<table class=\"table table-striped table-bordered\">\n";
 	$html .= "<tr>\n<th>ID</th>\n<th>Name</th>\n<th>Regulating Agency</th>\n<th>NAICS</th>\n";
 	$html .= "<th>Region</th>\n<th>Geography Type</th>\n<th>State</th>\n<th>Status</th>\n";
 	$html .= "<th>Deactivation Date</th>\n<th>PWS Type</th>\n<th>Souce</th>\n</tr>";
@@ -60,22 +47,96 @@ function resultsToTable($results) {
 	return $html . "</table>\n";
 }
 
-?>
+?><!DOCTYPE html>
+<html>
+	<head>
+		<title>EPA Data</title>
+		<meta charset="utf-8">
+		<title>Bootstrap, from Twitter</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<link href="css/bootstrap.css" rel="stylesheet" />
+		<link href="css/bootstrap-responsive.css" rel="stylesheet" />
+		<link href="css/core.css" rel="stylesheet" />
+		
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		  <script src="js/html5shiv.js"></script>
+		<![endif]-->
+	
+	</head>
+	
+	<body>
+		
+		<div class="navbar navbar-inverse navbar-fixed-top">
+		  <div class="navbar-inner">
+			<div class="container">
+			  <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="brand" href="#">EPA Water Purity</a>
+			  <div class="nav-collapse collapse">
+				<ul class="nav">
+				  <li class="active"><a href="#">Home</a></li>
+				  <li><a href="#about">About</a></li>
+				</ul>
+			  </div><!--/.nav-collapse -->
+			</div>
+		  </div>
+		</div>
+		
+		
 
-</head>
-
-<body>
-<h1>Active Public Water Systems in Georgia</h1>
-
-<?php
-
-// set the URL of the data request, and call the functions to consume and display data
-$url = 'http://iaspub.epa.gov/enviro/efservice/PWS/CONTACTSTATE/GA';
-echo 'test';
-$xmlResults = requestData($url);
-echo 'test';
-echo(resultsToTable($xmlResults));
-
-?>
-</body>
+		
+		<div class="container-fluid">
+			
+			<div class="row-fluid">
+				<div class="span4">
+					<h3>Enter your zip code:</h3>
+				</div>
+				<div class="span8">
+					<form class="form-search">
+						<input type="text" class="input-large search-query">
+						<button type="submit" class="btn btn-medium"><i class="icon-search"></i> Search</button>
+					</form>
+				</div>
+			</div>
+			
+			
+			<h1>Active Public Water Systems in Georgia</h1>
+			
+			<?php
+			
+			// set the URL of the data request, and call the functions to consume and display data
+			$url = 'http://iaspub.epa.gov/enviro/efservice/PWS/CONTACTSTATE/GA/rows/1:10';
+			echo 'test';
+			$xmlResults = requestData($url);
+			echo 'test';
+			echo(resultsToTable($xmlResults));
+			
+			?>
+		</div>
+		
+		<footer>
+			<img src="img/logo-epa.png" alt="Environmental Protection Agency" />
+			
+		</footer>
+		
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap-transition.js"></script>
+		<script src="js/bootstrap-alert.js"></script>
+		<script src="js/bootstrap-modal.js"></script>
+		<script src="js/bootstrap-dropdown.js"></script>
+		<script src="js/bootstrap-scrollspy.js"></script>
+		<script src="js/bootstrap-tab.js"></script>
+		<script src="js/bootstrap-tooltip.js"></script>
+		<script src="js/bootstrap-popover.js"></script>
+		<script src="js/bootstrap-button.js"></script>
+		<script src="js/bootstrap-collapse.js"></script>
+		<script src="js/bootstrap-carousel.js"></script>
+		<script src="js/bootstrap-typeahead.js"></script>
+	</body>
 </html>
