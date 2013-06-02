a218ce3514def9be7f95ae3f251f3d8fca329d79
Jason Doyle <jason.doyle@morris.com>
2013-06-02 03:10:31 -0400

    Visualization View
    Page for viewing visualization options

---
+++ public_html/visualizations.php
@@ -0,0 +1,39 @@
+<?php include 'inc/header.php'; ?>		
+		
+		<!-- Pie Chart  Container -->
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div id="pieChart"></div>
+				</div>
+			</div>
+		</div>
+ 
+          <!-- Bar Chart  Comtainer -->
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div id="barChart"></div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Scatter Chart  Comtainer -->
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div id="steppedChart"></div>
+				</div>
+			</div>
+		</div>
+				
+		<!-- DELETE - testing container -->
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<!-- Chart test Comtainer -->
+					<div id="raw_data" style="width:400; height:300"></div>
+				</div>
+			</div>
+		</div>
+<?php include 'inc/footer.php'; ?>
